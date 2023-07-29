285c964a88aeed1e5880726f9913c1dd1dff90b6
Dogboy21 <info@dogboy.io>
2023-07-29 18:56:41 +0200

    fix(modlauncher): add class locator so patched mods can find the patched OIS correctly

--- modlauncher/src/main/java/io/dogboy/serializationisbad/modlauncher/SerializationIsBadTransformationService.java
+++ modlauncher/src/main/java/io/dogboy/serializationisbad/modlauncher/SerializationIsBadTransformationService.java
@@ -6,10 +6,17 @@ import cpw.mods.modlauncher.api.ITransformer;
 import cpw.mods.modlauncher.api.IncompatibleEnvironmentException;
 import io.dogboy.serializationisbad.core.SerializationIsBad;
 
+import java.net.URL;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class SerializationIsBadTransformationService implements ITransformationService {
@@ -43,5 +50,27 @@ public class SerializationIsBadTransformationService implements ITransformationS
 
     @Override
     public void beginScanning(IEnvironment environment) { }
+
+    @Override
+    public Map.Entry<Set<String>, Supplier<Function<String, Optional<URL>>>> additionalClassesLocator() {
+        if (SerializationIsBad.isAgentActive()) return null;
+
+        return new Map.Entry<Set<String>, Supplier<Function<String, Optional<URL>>>>() {
+            @Override
+            public Set<String> getKey() {
+                return new HashSet<>(Arrays.asList("io.dogboy.serializationisbad."));
+            }
+
+            @Override
+            public Supplier<Function<String, Optional<URL>>> getValue() {
+                return () -> str -> Optional.ofNullable(SerializationIsBadTransformationService.class.getResource("/" + str));
+            }
+
+            @Override
+            public Supplier<Function<String, Optional<URL>>> setValue(Supplier<Function<String, Optional<URL>>> value) {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 
 }
